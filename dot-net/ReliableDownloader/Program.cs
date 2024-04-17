14a19ee0dbf77a7f763d81eac9498c5693510b36
robert.jones <robert.jones@accurx.com>
2024-04-17 13:46:40 +0100

    feat: Parameterise console app

--- dot-net/ReliableDownloader/Program.cs
+++ dot-net/ReliableDownloader/Program.cs
@@ -1,18 +1,27 @@
-namespace ReliableDownloader;
+using ReliableDownloader;
 
-internal class Program
+var exampleUrl = args.Length > 0
+    ? args[0]
+    // If this url 404's, you can get a live one from https://installer.demo.accurx.com/chain/latest.json.
+    : "https://installer.demo.accurx.com/chain/4.22.50587.0/accuRx.Installer.Local.msi";
+
+var exampleFilePath = args.Length > 1
+    ? args[1]
+    : Path.Combine(Directory.GetCurrentDirectory(), "myfirstdownload.msi");
+
+using var cts = new CancellationTokenSource();
+
+if (args.Length > 2)
 {
-    public static async Task Main(string[] args)
-    {
-        // If this url 404's, you can get a live one from https://installer.demo.accurx.com/chain/latest.json.
-        var exampleUrl = "https://installer.demo.accurx.com/chain/4.22.50587.0/accuRx.Installer.Local.msi";
-        var exampleFilePath = Path.Combine(Directory.GetCurrentDirectory(), "myfirstdownload.msi");
-        var fileDownloader = new FileDownloader();
-        var didDownloadSuccessfully = await fileDownloader.TryDownloadFile(
-            exampleUrl,
-            exampleFilePath,
-            progress => Console.WriteLine($"Percent progress is {progress.ProgressPercent}"),
-            CancellationToken.None);
-        Console.WriteLine($"File download ended! Success: {didDownloadSuccessfully}");
-    }
+    cts.CancelAfter(TimeSpan.FromMilliseconds(int.Parse(args[2])));
 }
+
+var fileDownloader = new FileDownloader();
+
+var didDownloadSuccessfully = await fileDownloader.TryDownloadFile(
+    exampleUrl,
+    exampleFilePath,
+    progress => Console.WriteLine($"Percent progress is {progress.ProgressPercent}"),
+    cts.Token);
+
+Console.WriteLine($"File download ended! Success: {didDownloadSuccessfully}");
