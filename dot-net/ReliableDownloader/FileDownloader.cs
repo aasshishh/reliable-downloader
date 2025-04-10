4d363bd70c16c71a04db6cc181aba7ca82bd526f
damien-odwyer <damien@accurx.com>
2025-04-10 15:08:44 +0100

    added basic full download implementation

--- dot-net/ReliableDownloader/FileDownloader.cs
+++ dot-net/ReliableDownloader/FileDownloader.cs
@@ -2,9 +2,34 @@
 
 internal sealed class FileDownloader : IFileDownloader
 {
-    public Task<bool> TryDownloadFile(
+    private readonly WebSystemCalls _webSystemCalls;
+
+    public FileDownloader(WebSystemCalls webSystemCalls)
+    {
+        _webSystemCalls = webSystemCalls;
+    }
+
+    public async Task<bool> TryDownloadFile(
         string contentFileUrl,
         string localFilePath,
         Action<FileProgress> onProgressChanged,
-        CancellationToken cancellationToken) => throw new NotImplementedException();
+        CancellationToken cancellationToken)
+    {
+        var headers = await _webSystemCalls.GetHeadersAsync(contentFileUrl, cancellationToken);
+        headers.EnsureSuccessStatusCode();
+        var supportsPartial = headers.Headers.AcceptRanges.Contains("bytes");
+        var tmpFilePath = $"{headers.Content.Headers.LastModified:yyyyMMddTHHmmss}.download";
+        HttpResponseMessage response;
+        if (supportsPartial == false)
+        {
+            response = await _webSystemCalls.DownloadContentAsync(contentFileUrl, cancellationToken);
+            await using var responseStream = await response.Content.ReadAsStreamAsync(cancellationToken);
+            var destination = File.Open(tmpFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            await response.Content.CopyToAsync(destination, null, cancellationToken).ConfigureAwait(continueOnCapturedContext: false);
+
+            return true;
+        }
+
+        throw new NotImplementedException();
+    }
 }
